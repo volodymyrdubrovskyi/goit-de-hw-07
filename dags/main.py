f33bd0b043e46f9d23789e4a9640848960a1f99c
volodymyrdubrovskyi <vladimir.dubrovskiy@gmail.com>
2024-12-07 22:09:19 +0200

    v.07.12.2024.22.09

--- dags/main.py
+++ dags/main.py
@@ -33,28 +33,29 @@ CREATE TABLE IF NOT EXISTS medal_count_vvd (
 """
 
 calculate_bronze_sql = """
-INSERT INTO medal_count (medal_type, count, created_at)
+INSERT INTO medal_count_vvd (medal_type, count, created_at)
 VALUES ('Bronze', (SELECT COUNT(*) FROM olympic_dataset.athlete_event_results WHERE medal = 'Bronze'), NOW());
 """
 
 calculate_silver_sql = """
-INSERT INTO medal_count (medal_type, count, created_at)
+INSERT INTO medal_count_vvd (medal_type, count, created_at)
 VALUES ('Silver', (SELECT COUNT(*) FROM olympic_dataset.athlete_event_results WHERE medal = 'Silver'), NOW());
 """
 
 calculate_gold_sql = """
-INSERT INTO medal_count (medal_type, count, created_at)
+INSERT INTO medal_count_vvd (medal_type, count, created_at)
 VALUES ('Gold', (SELECT COUNT(*) FROM olympic_dataset.athlete_event_results WHERE medal = 'Gold'), NOW());
 """
 
 # Функція для вибору випадкового типу медалі
-def pick_medal():
-    global medal_type
+def pick_medal(ti):
     medal_type = random.choice(['Bronze', 'Silver', 'Gold'])
+    ti.xcom_push(key='medal_type', value=medal_type)
     return medal_type
 
 # Розгалуження
-def check_medal_type():
+def check_medal_type(ti):
+    medal_type = ti.xcom_pull(key='medal_type', task_ids='pick_medal')
     if medal_type == 'Bronze':
         return 'calc_bronze_task'
     if medal_type == 'Silver':
@@ -63,7 +64,7 @@ def check_medal_type():
         return 'calc_gold_task'
 
 def wait():
-    time.sleep(35)
+    time.sleep(5)
 
 # Завдання DAG
 
@@ -83,6 +84,7 @@ choose_medal_task = PythonOperator(
 check_medal_type_task = BranchPythonOperator(
     task_id='pick_medal_task',
     python_callable=check_medal_type,
+    provide_context=True,
     dag=dag,
 )
 
@@ -111,6 +113,7 @@ generate_delay = PythonOperator(
     task_id='generate_delay',
     python_callable=wait,
     dag=dag,
+    trigger_rule='one_success',
 )
 
 check_for_correctness = SqlSensor(
@@ -118,16 +121,18 @@ check_for_correctness = SqlSensor(
     conn_id='goit_mysql_db_volodymyrdubrovskyi',
     sql="""
     SELECT COUNT(*) 
-    FROM medal_count 
+    FROM medal_count_vvd 
     WHERE created_at >= NOW() - INTERVAL 30 SECOND;
     """,
     timeout=60,
     poke_interval=5,
     mode='poke',
     dag=dag,
+    trigger_rule='one_success',
 )
 
 # Порядок завдань
 create_table_task >> choose_medal_task >> check_medal_type_task
 check_medal_type_task >> [calculate_bronze_task, calculate_silver_task, calculate_gold_task]
-[calculate_bronze_task, calculate_silver_task, calculate_gold_task] >> generate_delay >> check_for_correctness
+[calculate_bronze_task, calculate_silver_task, calculate_gold_task] >> generate_delay
+generate_delay >> check_for_correctness
